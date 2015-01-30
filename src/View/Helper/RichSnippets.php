b15ed4c1f81a8a7be44ce8a439b1b78d4318da21
NathanS <NathanS@harvest.org>
2015-01-30 09:27:40 -0800

    Added snippets helper

--- src/View/Helper/RichSnippets.php
+++ src/View/Helper/RichSnippets.php
@@ -2,17 +2,10 @@
 
 namespace NSCommons\View\Helper;
 
-use Zend\Form\View\Helper\AbstractHelper;
+use Zend\View\Helper\AbstractHelper;
 
 class RichSnippets extends AbstractHelper {
 	protected $data = array();
-
-	public function __invoke(array $data=null) {
-		if(isset($data)) {
-			$this->setData($data);
-		}
-		return $this;
-	}
 
 	public function render() {
 		$json = [
@@ -22,19 +15,63 @@ class RichSnippets extends AbstractHelper {
 		if(isset($this->data['title'])) $json['name'] = $this->data['title'];
 		if(isset($this->data['description'])) $json['description'] = $this->data['description'];
 		$html = '<script type="application/ld+json">' . json_encode($json) . '</script>';
+
+		if(isset($this->data['video'])) {
+			$view = $this->getView();
+			$html .= '<meta property="og:video" content="' . $view->escapeHtml($this->data['video']['url']) . '" />';
+			$html .= '<meta property="og:video:width" content="' . $view->escapeHtml($this->data['video']['width']) . '" />';
+			$html .= '<meta property="og:video:height" content="' . $view->escapeHtml($this->data['video']['height']) . '" />';
+
+/*
+			$html .= '
+			<div itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+				<span itemprop="name">' . $view->escapeHtml($this->data['video']['title']) . '</span>
+				<meta itemprop="duration" content="T1H3M33s" />
+				<meta itemprop="thumbnailUrl" content="' . $view->escapeHtml($this->data['video']['thumbnail_url']) . '" />
+				<meta itemprop="contentURL" content="' . $view->escapeHtml($this->data['video']['url']) . '" />
+			</div>
+			';
+			*/
+		}
+
 		return $html;
 	}
 
-	public function setData(array $data) {
-		if(isset($data['video'])) {
-			$view = $this->getView();
-			$headMeta = $view->headMeta();
-			$headMeta
-				->setProperty('og:video',$data['video'])
-				->setProperty('og:video:width',$data['video']['width'])
-				->setProperty('og:video:height',$data['video']['height']);
+	public function setVideo($url,$width,$height/*,$title=null,$thumbnail_url=null,$file_url=null*/) {
+		$video = [
+			'url' => $url,
+			'width' => $width,
+			'height' => $height/*,
+			'title' => $title,
+			'thumbnail_url' => $thumbnail_url,
+			'file_url' => $file_url*/
+		];
+		$this->data['video'] = $video;
+		return;
+	}
+	public function setTitle($title) {
+		$this->data['title'] = $title;
+		return;
+	}
+
+	public function setDescription($description) {
+		$this->data['description'] = $description;
+		return;
+	}
+
+	public function __invoke(array $data=null) {
+		if(isset($data)) {
+			$this->setData($data);
 		}
+		return $this;
+	}
+
+	public function setData(array $data) {
 		$this->data = $data;
 		return $this;
+	}
+
+	public function __toString() {
+		return $this->render();
 	}
 }
